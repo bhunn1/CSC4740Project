df9a461c9377ffcaa3ee970722fb8dc4915d4e24
dhdschool <dhdschool@outlook.com>
2025-03-21 14:59:21 -0500

    Add config instruction for local execution

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
+import pyspark
+import pyspark.sql
 from pyspark.sql import SparkSession
+import pyspark.ml.torch.distributor as distributer
 import pandas as pd
 import os
 from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
+import glob
+
+import pyspark.data
 
 # Opens and manages pyspark context, will run on localhost
 class SparkManager:
@@ -15,20 +22,32 @@ class SparkManager:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.context.stop()
        
- 
 def read_csv(sc: SparkSession, path: os.PathLike):
+    path = str(path)
+    
     df = sc.read.csv(path=path, inferSchema=True, header=True)
     return df
 
 def read_images(sc: SparkSession, path: os.PathLike):
+    path = str(path)
+    
     df = sc.read.format("image").load(path)
     return df
-
+    
 # Data is too large to put on git
 # the archive is unzipped in ./data/
 # link here: https://www.kaggle.com/datasets/ikarus777/best-artworks-of-all-time
 
-if __name__ == '__main__':
+# To run on local, driver memory limit needs to increase to accomodate file size
+# otherwise you might get a java lang out of memory error on the executor
+
+# To do this, go to your spark install conf 
+# (hopefully /opt/spark/conf/spark-defaults.conf, if the file isn't there just create it)
+# and add/modify the line to "spark.driver.memory 8g" to give the driver node 8GBs memory to work with
+# iirc this dataset took ~6GB when running
+
+
+if __name__ == '__main__':    
     with SparkManager(name='art') as sc:        
         # Example from the artists file
         csv = read_csv(sc, Path('data/artists.csv'))
@@ -36,11 +55,11 @@ if __name__ == '__main__':
         csv.printSchema()
         
         # Example from images
-        images = read_images(sc, Path('data/images/images/Raphael/*'))
+        images = read_images(sc, Path('data/images/images/**'))
         
         # Turns struct values into columns
         images = images.select('image.*')
-        images.show(3)        
+        images.show(10)        
         images.printSchema()
         
         
